7e27d20825533f89c33803a161a47c2e70183c13
md javed <md javed@SKF100268>
2018-09-09 03:06:06 -0400

    Method made Generic

--- ShopifyApiData/Controllers/HomeController.cs
+++ ShopifyApiData/Controllers/HomeController.cs
@@ -10,50 +10,26 @@ using System.IO;
 using Newtonsoft.Json;
 using ShopifyApiData.Models;
 
+using ShopifyApiData.Core;
+
 namespace ShopifyApiData.Controllers
 {
   public class HomeController : Controller
   {
     private string BaseUrl = "https://8f1814591b650441af166fff21eedd2e:8ed8d6d70fe5c6c5e705a4454d3f79c0@skfdev.myshopify.com/";
-    Uri BaseUri = new Uri("https://8f1814591b650441af166fff21eedd2e:8ed8d6d70fe5c6c5e705a4454d3f79c0@skfdev.myshopify.com/admin/customers.json");
+    Uri BaseUri = new Uri("https://8f1814591b650441af166fff21eedd2e:8ed8d6d70fe5c6c5e705a4454d3f79c0@skfdev.myshopify.com/admin/");
+    //private T resultData;
     public  ActionResult IndexAsync()
     {
 
-      var a = gethttpResponse(BaseUri);
-      List<Customer> customers = a.Customer.ToList();
-
+      var abc = CustomerResponse.gethttpResponse<Customers>(BaseUri,"customers.json");
+      //var a =typeof(HomeController).gethttpResponse(BaseUri);
+      ////List<Customer> customers = a.Customer.ToList();
+      var customers = abc.Customer.ToList();//a.Customer.ToList();
+      //return View(customers);
       return View(customers);
     }
-    public static Customers gethttpResponse(Uri url)
-    {
-      HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-      req.Method = "GET";
-      req.ContentType = "application/json";
-      //req.Headers.Add("X-Shopify-Access-Token", APISecrateKey);
-      req.Credentials = new NetworkCredential("8f1814591b650441af166fff21eedd2e", "8ed8d6d70fe5c6c5e705a4454d3f79c0");
-      string text = string.Empty;
-      
-      
-        var response = (HttpWebResponse)req.GetResponse();
-        var stringResponse = new StreamReader(response.GetResponseStream());
-        text = stringResponse.ReadToEnd();
-        var abc = JsonConvert.DeserializeObject<Customers>(text);
-      return abc;
-      //using (var sr = new StreamReader(response.GetResponseStream()))
-      //{
-      //  text = sr.ReadToEnd();
-      //  var def = JsonConvert.DeserializeObject(text);
-      // var abc = JsonConvert.DeserializeObject<Customers>(text);
-
-      //}
-   // }
-      //catch (Exception a)
-      //{
-      //  //Utility.LogMessage(a.ToString());
-      //}
-      
-      // return text;
-    }
+   
     public ActionResult About()
     {
       ViewBag.Message = "Your application description page.";
---
+++ ShopifyApiData/Core/CustomerResponse.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ShopifyApiData.Core
+{
+  public static class CustomerResponse
+  {
+    public static T gethttpResponse<T>(Uri url, string input) where T : class
+    {
+      HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url + input);
+      req.Method = "GET";
+      req.ContentType = "application/json";
+      //req.Headers.Add("X-Shopify-Access-Token", APISecrateKey);
+      req.Credentials = new NetworkCredential("8f1814591b650441af166fff21eedd2e", "8ed8d6d70fe5c6c5e705a4454d3f79c0");
+      string text = string.Empty;
+      var response = (HttpWebResponse)req.GetResponse();
+      var stringResponse = new StreamReader(response.GetResponseStream());
+      text = stringResponse.ReadToEnd();
+      var abc = JsonConvert.DeserializeObject<T>(text);
+      return (T)abc;
+
+    }
+  }
+}
